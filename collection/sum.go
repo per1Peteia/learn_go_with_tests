d50df609c6b57fad3c3cbe43eeb94a5630e6b3b9
per1Peteia <justusmanthey@googlemail.com>
2025-03-14 12:27:34 +0100

    TestSumAllTails

--- collection/sum.go
+++ collection/sum.go
@@ -7,3 +7,23 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(slices ...[]int) []int {
+	var sums []int
+	for _, numbers := range slices {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+	for _, numbers := range slices {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return sums
+}
