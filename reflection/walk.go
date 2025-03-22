8693671c4255e21a9313808bafda74a0c4370cca
per1Peteia <justusmanthey@googlemail.com>
2025-03-22 22:21:54 +0100

    reflection/ :: feat: reflection function + testing it

---
+++ reflection/walk.go
@@ -0,0 +1,17 @@
+package main
+
+import "reflect"
+
+func walk(x interface{}, fn func(in string)) {
+	val := reflect.ValueOf(x)
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+	}
+}
