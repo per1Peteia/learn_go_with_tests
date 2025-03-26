aeb464673d9ea5f6e672193dfe0bcee99064b698
per1Peteia <justusmanthey@googlemail.com>
2025-03-26 16:58:27 +0100

    properties/ :: feat: added property based tests for conversion, subtractive numerals and consecutive numerals rules

--- 14_property_based_tests/roman_numerals.go
+++ 14_property_based_tests/roman_numerals.go
@@ -2,28 +2,48 @@ package main
 
 import "strings"
 
-func ConvertToRoman(number int) string {
+type RomanNumeral struct {
+	Value  uint16
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic uint16) string {
 	var result strings.Builder
-	for number > 0 {
-		switch {
-		case number > 9:
-			result.WriteString("X")
-			number -= 10
-		case number > 8:
-			result.WriteString("IX")
-			number -= 9
-		case number > 4:
-			result.WriteString("V")
-			number -= 5
-		case number > 3:
-			result.WriteString("IV")
-			number -= 4
-		default:
-			result.WriteString("I")
-			number--
+
+	for _, numeral := range allRomanNumerals {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
 
 	return result.String()
 }
 
+func ConvertToArabic(roman string) uint16 {
+	var arabic = uint16(0)
+
+	for _, numeral := range allRomanNumerals {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			arabic += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+
+	return arabic
+}
--- 14_property_based_tests/roman_numerals_test.go
+++ 14_property_based_tests/roman_numerals_test.go
@@ -1,38 +1,124 @@
 package main
 
 import (
+	"fmt"
+	"strings"
 	"testing"
+	"testing/quick"
 )
 
+var cases = []struct {
+	Arabic uint16
+	Roman  string
+}{
+	{1, "I"},
+	{2, "II"},
+	{3, "III"},
+	{4, "IV"},
+	{5, "V"},
+	{6, "VI"},
+	{7, "VII"},
+	{8, "VIII"},
+	{9, "IX"},
+	{10, "X"},
+	{10, "X"},
+	{14, "XIV"},
+	{18, "XVIII"},
+	{20, "XX"},
+	{39, "XXXIX"},
+	{40, "XL"},
+	{47, "XLVII"},
+	{49, "XLIX"},
+	{50, "L"},
+	{100, "C"},
+	{140, "CXL"},
+	{900, "CM"},
+	{1006, "MVI"},
+}
+
 func TestRomanNumerals(t *testing.T) {
-	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
-	}{
-		{"1 becomes I", 1, "I"},
-		{"2 becomes II", 2, "II"},
-		{"3 becomes III", 3, "III"},
-		{"4 becomes IV (can't repeat more than 3 times)", 4, "IV"},
-		{"5 becomes V", 5, "V"},
-		{"6 becomes VI", 6, "VI"},
-		{"7 becomes VII", 7, "VII"},
-		{"8 becomes VIII", 8, "VIII"},
-		{"9 becomes IX", 9, "IX"},
-		{"10 becomes X", 10, "X"},
-		{"10 gets converted to X", 10, "X"},
-		{"14 gets converted to XIV", 14, "XIV"},
-		{"18 gets converted to XVIII", 18, "XVIII"},
-		{"20 gets converted to XX", 20, "XX"},
-		{"39 gets converted to XXXIX", 39, "XXXIX"},
-	}
 
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			if got != test.Want {
-				t.Errorf("got %q, want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got %q, want %q", got, test.Roman)
 			}
 		})
+	}
+}
+
+func TestConvertToArabic(t *testing.T) {
+	for _, test := range cases[:1] {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got %d, want %d", got, test.Arabic)
+			}
+		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return arabic == fromRoman
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
+	}
+}
+
+func TestPropertiesOfSubtractives(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+
+		for i := 0; i < len(roman)-1; i++ {
+			current := roman[i]
+			next := roman[i+1]
+
+			if ConvertToArabic(string(current)) < ConvertToArabic(string(next)) {
+				if current != 'I' && current != 'X' && current != 'C' {
+					return false
+				}
+			}
+		}
+		return true
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
+	}
+}
+
+func TestPropertiesOfConsecutives(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+		if len(roman) > 3 {
+			for _, numeral := range allRomanNumerals {
+				if strings.Count(roman, numeral.Symbol) > 3 {
+					return false
+				}
+			}
+		}
+		return true
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
 	}
 }
