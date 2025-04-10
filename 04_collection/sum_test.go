83358ba1af6d5489cdd12b1929cc29d091e72ac1
per1Peteia <justusmanthey@googlemail.com>
2025-04-10 15:35:48 +0200

    tests: pass

--- 04_collection/sum_test.go
+++ 04_collection/sum_test.go
@@ -5,6 +5,25 @@ import (
 	"testing"
 )
 
+func TestBadBank(t *testing.T) {
+	transactions := []Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		},
+		{
+			From: "Adil",
+			To:   "Chris",
+			Sum:  25,
+		},
+	}
+
+	AssertEqual(t, BalanceFor(transactions, "Riya"), 100)
+	AssertEqual(t, BalanceFor(transactions, "Chris"), -75)
+	AssertEqual(t, BalanceFor(transactions, "Adil"), -25)
+}
+
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
